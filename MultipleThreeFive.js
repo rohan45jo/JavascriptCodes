d36ec1d4015c0df5348e573bacda31d08af02612
Rohan S Joshi <rohan.joshi@hashcube.com>
2018-06-22 13:27:21 +0530

    changed coding style to improve readability

--- MultipleThreeFive.js
+++ MultipleThreeFive.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var i = 1,
   sum = 0,
   max = 1000;
@@ -9,12 +8,3 @@ for(i = 1; i < max; i++) {
   }
 }
 console.log(sum);
-=======
-var i,sum = 0;
-for(i=1;i<1000;i++){
-  if(i%3 == 0 || i%5 == 0){
-    sum = sum + i;
-  }
-}
-console.log(sum);
->>>>>>> 7da2ef2194e9efa9a32a926d5e8d1e62efd12de7
